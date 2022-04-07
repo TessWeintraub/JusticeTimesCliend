a854d41f75bcd6c13c24caf6a96ee3afe19e5b0d
TheHaxy <vlad89896135637@mail.ru>
2022-04-08 00:23:26 +0300

    fix port

--- src/store/instances.js
+++ src/store/instances.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "https://5.181.109.106:5000/api/",
+  baseURL: "5.181.109.106:5000/api/",
   headers: {'Content-Type': 'application/json;charset=utf-8'},
 })
